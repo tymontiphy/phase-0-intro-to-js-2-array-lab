bcbdc1836ba511d0e50c0e0241c1dbc7f879670c
Your Name <youremail@domain.com>
2024-09-19 18:48:45 +0300

    creating arrays

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution here!
+let cats = ["Milo","Otis", "Garfield"];
+
+function destructivelyAppendCat(Ralph){
+    cats.push(Ralph);
+}
+function destructivelyPrependCat(Bob){
+    cats.unshift(Bob);
+}
+function destructivelyRemoveLastCat(Garfield){
+    cats.pop(Garfield);
+}
+
+function destructivelyRemoveFirstCat(Milo) {
+  cats.shift(Milo);
+}
+function appendCat(Broom){
+    return [...cats, Broom];
+}
+function prependCat(Arnold){
+    return [Arnold,...cats];
+}
+function removeLastCat(){
+    return cats.slice(0, -1);
+}
+function removeFirstCat(){
+    return cats.slice(1);
+}
